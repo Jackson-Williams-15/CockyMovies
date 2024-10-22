17fd2f30f284b4063e1a37025f8c4eb04ae420e0
Jackson Williams <wjack1201@yahoo.com>
2024-10-21 20:31:06 -0400

    Movie controller, mapping movieDtos

---
+++ CM.API/CM.API/controllers/MovieController.cs
@@ -0,0 +1,101 @@
+using CM.API.Interfaces;
+using CM.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace CM.API.Controllers;
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MoviesController : ControllerBase
+    {
+        private readonly IMovieService _movieService;
+
+        public MoviesController(IMovieService movieService)
+        {
+            _movieService = movieService;
+        }
+
+        // GET: api/movies
+        [HttpGet]
+        public IActionResult GetMovies()
+        {
+            var movies = _movieService.GetMovies();
+
+            // map movie entities to movieDtos
+            var movieDtos = movies.Select(m => new MovieDto
+            {
+                Id = m.Id,
+                Title = m.Title,
+                Description = m.Description,
+                DateReleased = m.DateReleased,
+                Genres = m.Genres.Select(g => new GenreDto
+                {
+                    Id = g.Id,
+                    Name = g.Name
+                }).ToList()
+            }).ToList();
+
+            return Ok(movieDtos);
+        }
+
+        // GET: api/movies/{id}
+        [HttpGet("{id}")]
+        public IActionResult GetMovieById(int id)
+        {
+            var movie = _movieService.GetMovieById(id);
+
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            var movieDto = new MovieDto
+            {
+                Id = movie.Id,
+                Title = movie.Title,
+                Description = movie.Description,
+                DateReleased = movie.DateReleased,
+                Genres = movie.Genres.Select(g => new GenreDto
+                {
+                    Id = g.Id,
+                    Name = g.Name
+                }).ToList()
+            };
+
+            return Ok(movieDto);
+        }
+
+        // POST: api/movies
+        [HttpPost]
+       public IActionResult AddMovie([FromBody] MovieCreateDto movieDto)
+        {
+            // genres by their IDs
+            var genres = _movieService.GetGenresByIds(movieDto.GenreIds);
+            
+            if (!genres.Any())
+            {
+                return BadRequest("Invalid genre IDs provided.");
+            }
+
+            // map MovieCreateDto to Movie entity and related genres
+            var movie = new Movie
+            {
+                Title = movieDto.Title,
+                Description = movieDto.Description,
+                DateReleased = movieDto.DateReleased,
+                // assign the genres to the movie
+                Genres = genres
+            };
+
+            var success = _movieService.AddMovie(movie);
+
+            if (!success)
+            {
+                return BadRequest("A movie with the same ID already exists.");
+            }
+
+            return Ok("Movie added successfully.");
+    }
+}
+
