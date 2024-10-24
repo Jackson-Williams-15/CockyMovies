7381c971270cc22248f248fac3ed1775757a0a60
Jackson Williams <wjack1201@yahoo.com>
2024-10-24 15:30:33 -0400

    ticket model for showtimes

---
+++ CM.API/CM.API/Models/Ticket.cs
@@ -0,0 +1,11 @@
+namespace CM.API.Models;
+
+public class Ticket {
+public int Id { get; set; }
+    public decimal Price { get; set; }
+    
+    // Foreign key for the associated showtime
+    public int ShowtimeId { get; set; }
+    public required Showtime Showtime { get; set; }
+
+}
