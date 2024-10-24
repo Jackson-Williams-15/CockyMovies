d219a3067d20b95e866875305164973d84fac1e3
Jackson Williams <wjack1201@yahoo.com>
2024-10-23 23:50:35 -0400

    Account Controller

---
+++ CM.API/CM.API/Controllers/AccountController.cs
@@ -0,0 +1,36 @@
+using System.Threading.Tasks;
+using CM.API.Interfaces;
+using CM.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CM.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AccountController : ControllerBase
+    {
+        private readonly IAccountService _accountService;
+
+        public AccountController(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+        [HttpPost]
+        public async IActionResult SignUp([FromBody] SignUpRequest signupRequest)
+        {
+            var user = await _accountService.Authenticate(signupRequest.Username, signupRequest.Password);
+
+            if (user == null)
+                return Unauthorized(new { message = "Invalid username or password" });
+
+            return Ok(user);
+        }
+    }
+
+    public class SignUpRequest
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
