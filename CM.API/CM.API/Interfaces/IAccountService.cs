d982b3bca008209414ebdb1ddd1e68694423f9b4
Jackson Williams <wjack1201@yahoo.com>
2024-10-24 15:06:43 -0400

    update account service and interface

--- CM.API/CM.API/Interfaces/IAccountService.cs
+++ CM.API/CM.API/Interfaces/IAccountService.cs
@@ -4,4 +4,5 @@ namespace CM.API.Interfaces;
     public interface IAccountService
     {
         Task<User> Authenticate(string username, string password);
+        Task<User> Register(string email, string username, string password, DateTime dateOfBirth);
     }
--- CM.API/CM.API/Services/AccountService.cs
+++ CM.API/CM.API/Services/AccountService.cs
@@ -2,6 +2,8 @@ using CM.API.Data;
 using CM.API.Interfaces;
 using CM.API.Models;
 using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using BCrypt.Net;
 
 namespace CM.API.Services;
 
@@ -17,14 +19,32 @@ namespace CM.API.Services;
         public async Task<User> Authenticate(string username, string password)
         {
             var user = await _context.Users
-                .Where(u => u.Username == username && u.Password == password)
+                .Where(u => u.Username == username)
                 .FirstOrDefaultAsync();
 
-            // Return null if user not found
-            if (user == null)
+            // Return null if user not found or password does not match
+            if (user == null || !BCrypt.Net.BCrypt.Verify(password, user.Password))
                 return null;
 
             // Authentication successful
+            return user;
+        }
+
+        public async Task<User> Register(string email, string username, string password, DateTime dateOfBirth)
+        {
+            var hashedPassword = BCrypt.Net.BCrypt.HashPassword(password);
+
+            var user = new User
+            {
+                Email = email,
+                Username = username,
+                Password = hashedPassword,
+                DateOfBirth = dateOfBirth
+            };
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
             return user;
         }
     }
