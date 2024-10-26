36f1ce0f76b9deb29601905be6c9dd8ffc5f9471
Jackson Williams <wjack1201@yahoo.com>
2024-10-26 16:14:19 -0400

    Mui Card component

--- CM.SPA/src/components/pages/Movies.jsx
+++ CM.SPA/src/components/pages/Movies.jsx
@@ -1,7 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { getMovies } from '../../Services/movieService';
-
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import CardActions from '@mui/material/CardActions';
+import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
 export default function Movies() {
   const [movies, setMovies] = useState([]);
 
@@ -21,23 +28,54 @@ export default function Movies() {
   return (
     <div>
       <h2>Movies List</h2>
-      <ul>
+      <Box sx={{ padding: 3 }}>
+      <Grid container spacing={3}>
         {movies.map(movie => (
-          <li key={movie.id}>
-            <h3>
-              <Link to={`/movies/${movie.id}/showtimes`}>{movie.title}</Link>
-            </h3>
-            <p>{movie.description}</p>
-            <h4>
-              {movie.genres && movie.genres.length > 0 ? (
-                movie.genres.map(genre => genre.name).join(', ')
-              ) : (
-                'No genres available'
-              )}
-            </h4>
-          </li>
+          <Grid item xs={12} sm={6} md={4} key={movie.id}>
+            <Card sx={{ height: 350, display: 'flex', flexDirection: 'column' }}>
+              <CardMedia
+                component="img"
+                alt={movie.title}
+                height="140"
+                image={movie.imageUrl}
+              />
+              <CardContent sx={{ flexgrow: '1' }}>
+                <Typography gutterBottom variant="h5" component="div">
+                  {movie.title}
+                </Typography>
+                <Typography gutterBottom variant="h6" component="div">
+                {movie.genres && movie.genres.length > 0
+                    ? movie.genres.map(genre => genre.name).join(', ')
+                    : 'No genres available'}
+                </Typography>
+                <Typography variant="body2" 
+                ariant="body2" sx={{
+                  maxHeight: 80,
+                  overflow: 'hidden',
+                  textOverflow: 'ellipsis',
+                  display: '-webkit-box',
+                  WebkitLineClamp: 3,
+                  WebkitBoxOrient: 'vertical',
+                  color: 'text.secondary'
+              }}>
+                  {movie.description}
+                </Typography>
+                {/*<Typography variant="subtitle2" color="text.secondary">
+                  {movie.genres && movie.genres.length > 0
+                    ? movie.genres.map(genre => genre.name).join(', ')
+                    : 'No genres available'}
+                </Typography>*/}
+              </CardContent>
+              <CardActions>
+                <Button size="large" component={Link} to={`/movies/${movie.id}/showtimes`}>
+                  Show Times
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
         ))}
-      </ul>
+      </Grid>
+      </Box>
     </div>
   );
 }
