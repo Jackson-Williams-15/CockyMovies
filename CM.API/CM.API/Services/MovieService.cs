ca171267fa5cf75e5e51c41a921517a8ab76d56f
Jackson Williams <wjack1201@yahoo.com>
2024-10-26 16:21:38 -0400

    Added the ImageURL to movie service

--- CM.API/CM.API/Services/MovieService.cs
+++ CM.API/CM.API/Services/MovieService.cs
@@ -25,6 +25,11 @@ namespace CM.API.Services
 
             movie.Id = _movies.Count > 0 ? _movies.Max(m => m.Id) + 1 : 1;
 
+            //Set the ImageUrl while adding the movie
+    if (string.IsNullOrWhiteSpace(movie.ImageUrl))
+    {
+     movie.ImageUrl =  "https://plus.unsplash.com/premium_vector-1682303466154-2161da750ac7?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fG1vdmllfGVufDB8fDB8fHww";
+    }
             // add movie in-memory
             _movies.Add(movie);
             return true;
@@ -32,12 +37,36 @@ namespace CM.API.Services
 
         public Movie? GetMovieById(int id)
         {
-            return _movies.FirstOrDefault(m => m.Id == id);
+            var movie = _movies.FirstOrDefault(m => m.Id == id);
+            if (movie != null)
+            {
+                // Return the movie with all properties
+                return new Movie
+                {
+                    Id = movie.Id,
+                    Title = movie.Title,
+                    Description = movie.Description,
+                    DateReleased = movie.DateReleased,
+                    Genres = movie.Genres,
+                    Showtimes = movie.Showtimes,
+                    ImageUrl = movie.ImageUrl
+                };
+            }
+            return null;
         }
 
         public List<Movie> GetMovies()
         {
-            return _movies;
+            return _movies.Select(movie => new Movie
+            {
+                Id = movie.Id,
+                Title = movie.Title,
+                Description = movie.Description,
+                DateReleased = movie.DateReleased,
+                Genres = movie.Genres,
+                Showtimes = movie.Showtimes,
+                ImageUrl = movie.ImageUrl
+            }).ToList();
         }
 
         public List<Genre> GetGenresByIds(List<int> genreIds)
