755078ba22b895bd17dd81d37d2dd9a7b1bfb73a
Jackson Williams <wjack1201@yahoo.com>
2024-10-29 00:16:14 -0400

    added auth provider, got rid of unneeded imports

--- CM.SPA/src/App.jsx
+++ CM.SPA/src/App.jsx
@@ -1,8 +1,4 @@
-import { useState } from 'react';
 import Navbar from './components/Navbar';
-import CssBaseline from '@mui/material/CssBaseline';
-import Box from '@mui/material/Box';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import { ThemeProvider } from '@mui/material/styles';
 import { Route, Routes, Link } from 'react-router-dom';
 import Home from './components/pages/Home';
@@ -10,11 +6,14 @@ import About from './components/pages/About';
 import Movies from './components/pages/Movies';
 import Showtimes from './components/pages/Showtimes';
 import Cart from './components/pages/Cart';
-import './components/Navbar.css';
 import Theme from './Theme';
+import SignIn from './components/pages/SignIn';
+import Signup from './components/pages/Signup';
+import { AuthProvider } from './context/AuthContext';
+
 function App() {
   return (
-    <div>
+    <AuthProvider>
       <ThemeProvider theme={Theme}>
         <Navbar />
         <Routes>
@@ -23,9 +22,11 @@ function App() {
           <Route path="/movies" element={<Movies />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/movies/:movieId/showtimes" element={<Showtimes />} />
+          <Route path="/login" element={<SignIn />} />
+          <Route path="/signup" element={<Signup />} />
         </Routes>
       </ThemeProvider>
-    </div>
+    </AuthProvider>
   );
 }
 
