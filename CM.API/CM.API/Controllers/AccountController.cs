578440cb4d2eb7b49d0797424d4586df1b7eac73
Jackson Williams <wjack1201@yahoo.com>
2024-10-29 00:04:42 -0400

    added tokens to controller

--- CM.API/CM.API/Controllers/AccountController.cs
+++ CM.API/CM.API/Controllers/AccountController.cs
@@ -1,7 +1,14 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using CM.API.Interfaces;
 using CM.API.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace CM.API.Controllers
 {
@@ -10,13 +17,16 @@ namespace CM.API.Controllers
     public class AccountController : ControllerBase
     {
         private readonly IAccountService _accountService;
+        private readonly IConfiguration _configuration;
 
-        public AccountController(IAccountService accountService)
+        public AccountController(IAccountService accountService, IConfiguration configuration)
         {
             _accountService = accountService;
+            _configuration = configuration;
         }
 
         [HttpPost("signup")]
+        [AllowAnonymous]
         public async Task<IActionResult> SignUp([FromBody] UserCreateDto signupRequest)
         {
             var user = await _accountService.Register(signupRequest.Email, signupRequest.Username, signupRequest.Password, signupRequest.DateOfBirth);
@@ -36,12 +46,15 @@ namespace CM.API.Controllers
         }
 
         [HttpPost("login")]
+        [AllowAnonymous]
         public async Task<IActionResult> Login([FromBody] UserLoginDto loginRequest)
         {
             var user = await _accountService.Authenticate(loginRequest.Username, loginRequest.Password);
 
             if (user == null)
                 return Unauthorized(new { message = "Invalid username or password" });
+
+            var token = GenerateJwtToken(user);
 
             var userDto = new UserDto
             {
@@ -51,7 +64,28 @@ namespace CM.API.Controllers
                 DateOfBirth = user.DateOfBirth
             };
 
-            return Ok(userDto);
+            return Ok(new { token, user = userDto });
+        }
+
+        private string GenerateJwtToken(User user)
+        {
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.Username),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["Jwt:Issuer"],
+                audience: _configuration["Jwt:Issuer"],
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(30),
+                signingCredentials: creds);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
         }
     }
 }
