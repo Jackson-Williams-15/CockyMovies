ef5a5426b052860ee7ab116277391704787b7b66
Jackson Williams <wjack1201@yahoo.com>
2024-10-29 00:13:54 -0400

    Auth context and provider

---
+++ CM.SPA/src/context/AuthContext.jsx
@@ -0,0 +1,65 @@
+import React, { createContext, useState, useEffect } from 'react';
+import { login, signup, logout } from '../Services/authService';
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    const userData = localStorage.getItem('user');
+    if (token && userData) {
+      setIsAuthenticated(true);
+      setUser(JSON.parse(userData));
+    }
+  }, []);
+
+  // handles logins 
+  const handleLogin = async (credentials) => {
+    try {
+      const data = await login(credentials);
+      if (data.token) {
+        localStorage.setItem('token', data.token);
+        localStorage.setItem('user', JSON.stringify(data.user));
+        setIsAuthenticated(true);
+        setUser(data.user);
+      } else {
+        console.error('AuthContext - No token received');
+      }
+    } catch (error) {
+      console.error('AuthContext - Login error:', error);
+    }
+  };
+
+  // handles sign ups
+  const handleSignup = async (credentials) => {
+    try {
+      const data = await signup(credentials);
+      if (data.token) {
+        localStorage.setItem('token', data.token);
+        localStorage.setItem('user', JSON.stringify(data.user));
+        setIsAuthenticated(true);
+        setUser(data.user);
+      } else {
+        console.error('AuthContext - No token received');
+      }
+    } catch (error) {
+      console.error('AuthContext - Signup error:', error);
+    }
+  };
+
+  // handles log outs
+  const handleLogout = () => {
+    logout();
+    setIsAuthenticated(false);
+    setUser(null);
+  };
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, user, handleLogin, handleSignup, handleLogout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
