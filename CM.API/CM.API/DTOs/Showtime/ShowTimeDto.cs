93082526aa67cf4f8b8ba4df6bff148d16c24fe9
Jackson Williams <wjack1201@yahoo.com>
2024-11-01 17:04:30 -0400

    initialize tickets

--- CM.API/CM.API/DTOs/Showtime/ShowTimeDto.cs
+++ CM.API/CM.API/DTOs/Showtime/ShowTimeDto.cs
@@ -2,7 +2,8 @@ public class ShowtimeDto
 {
     public int Id { get; set; }
     public DateTime StartTime { get; set; }
-    public List<TicketDto> Tickets { get; set; }
+    public List<TicketDto> Tickets { get; set; } = new List<TicketDto>();
     public int AvailableTickets => Tickets.Count;
     public decimal TicketPrice => Tickets.FirstOrDefault()?.Price ?? 0;
+    public MovieDto Movie { get; set; }
 }
