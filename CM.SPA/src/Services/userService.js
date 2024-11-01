eec20dbf99a6dec5ef4ce88c2954fc2f23d239ab
Jackson Williams <wjack1201@yahoo.com>
2024-11-01 12:57:38 -0400

    change name

---
+++ CM.SPA/src/Services/userService.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+const BASE_API_URL = '/api/Account';
+
+// Get user information
+const getUserInfo = async () => {
+  const token = localStorage.getItem('token');
+  if (!token) {
+    throw new Error('No token found');
+  }
+
+  try {
+    const response = await axios.get(`${BASE_API_URL}/profile`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error(
+      'Error fetching user data:',
+      error.response ? error.response.data : error.message,
+    );
+    throw error;
+  }
+};
+
+// Export the service functions
+export default {
+  getUserById,
+};
