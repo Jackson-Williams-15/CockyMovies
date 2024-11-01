59d65f714e4c0ca61650c0f4948d020bb50d58a9
Jackson Williams <wjack1201@yahoo.com>
2024-11-01 17:04:39 -0400

    user have cart

--- CM.API/CM.API/DTOs/UserDto.cs
+++ CM.API/CM.API/DTOs/UserDto.cs
@@ -1,7 +1,9 @@
+using CM.API.Models;
 public class UserDto
 {
     public int Id { get; set; }
     public string Email { get; set; }
     public string Username { get; set; }
     public DateTime DateOfBirth { get; set; }
+    public Cart Cart { get; set; }
 }
