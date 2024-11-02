38b051ee114f41afc93027a89c96d2b83f88c991
Jackson Williams <wjack1201@yahoo.com>
2024-11-02 19:12:59 -0400

    Uses async

--- CM.API/CM.API/Controllers/TicketController.cs
+++ CM.API/CM.API/Controllers/TicketController.cs
@@ -2,6 +2,8 @@ using CM.API.Interfaces;
 using CM.API.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace CM.API.Controllers
 {
@@ -18,14 +20,14 @@ namespace CM.API.Controllers
 
         // POST: api/tickets
         [HttpPost]
-        public IActionResult AddTicket([FromBody] Ticket ticket)
+        public async Task<IActionResult> AddTicket([FromBody] Ticket ticket)
         {
             if (ticket == null)
             {
                 return BadRequest("Ticket cannot be null.");
             }
 
-            if (_ticketService.AddTicket(ticket))
+            if (await _ticketService.AddTicket(ticket))
             {
                 return CreatedAtAction(nameof(GetTicketById), new { id = ticket.Id }, ticket);
             }
@@ -35,17 +37,17 @@ namespace CM.API.Controllers
 
         // GET: api/tickets
         [HttpGet]
-        public IActionResult GetAllTickets()
+        public async Task<IActionResult> GetAllTickets()
         {
-            var tickets = _ticketService.GetAllTickets();
+            var tickets = await _ticketService.GetAllTickets();
             return Ok(tickets);
         }
 
         // GET: api/tickets/by-movie/{movieId}
         [HttpGet("by-movie/{movieId}")]
-        public IActionResult GetTicketsByMovieId(int movieId)
+        public async Task<IActionResult> GetTicketsByMovieId(int movieId)
         {
-            var tickets = _ticketService.GetTicketsByMovieId(movieId);
+            var tickets = await _ticketService.GetTicketsByMovieId(movieId);
             if (tickets == null || !tickets.Any())
             {
                 return NotFound("No tickets found for this movie.");
@@ -56,9 +58,9 @@ namespace CM.API.Controllers
 
         // GET: api/tickets/{id}
         [HttpGet("{id}")]
-        public IActionResult GetTicketById(int id)
+        public async Task<IActionResult> GetTicketById(int id)
         {
-            var ticket = _ticketService.GetAllTickets().FirstOrDefault(t => t.Id == id);
+            var ticket = await _ticketService.GetTicketById(id);
             if (ticket == null)
             {
                 return NotFound("Ticket not found.");
