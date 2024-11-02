5b3d212bb78e6ce734c627f3637766dd26fefea6
Jackson Williams <wjack1201@yahoo.com>
2024-11-02 19:28:34 -0400

    fix showtime Url

--- CM.SPA/src/Services/showtimeService.js
+++ CM.SPA/src/Services/showtimeService.js
@@ -1,5 +1,5 @@
 export async function getShowtimes(movieId) {
-  const BASE_API_URL = `/api/showtimes`;
+  const BASE_API_URL = `/api/Showtimes`;
 
   const response = await fetch(`${BASE_API_URL}/movie/${movieId}`);
   if (!response.ok) {
