6861a2e2be5666aaa97d3d177ccdc3d744cc1535
Jackson Williams <wjack1201@yahoo.com>
2024-11-02 18:54:36 -0400

    migrations

---
+++ CM.API/CM.API/Migrations/20241101190105_AddCartAndUpdateUserModel.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CM.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCartAndUpdateUserModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CartId",
+                table: "Ticket",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Carts",
+                columns: table => new
+                {
+                    CartId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Carts", x => x.CartId);
+                    table.ForeignKey(
+                        name: "FK_Carts_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ticket_CartId",
+                table: "Ticket",
+                column: "CartId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Carts_UserId",
+                table: "Carts",
+                column: "UserId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Ticket_Carts_CartId",
+                table: "Ticket",
+                column: "CartId",
+                principalTable: "Carts",
+                principalColumn: "CartId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Ticket_Carts_CartId",
+                table: "Ticket");
+
+            migrationBuilder.DropTable(
+                name: "Carts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Ticket_CartId",
+                table: "Ticket");
+
+            migrationBuilder.DropColumn(
+                name: "CartId",
+                table: "Ticket");
+        }
+    }
+}
