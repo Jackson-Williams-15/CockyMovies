99b51f416d446e4af3e1daf05dbe1f3b2185cd89
Jackson Williams <wjack1201@yahoo.com>
2024-11-03 11:01:09 -0500

    migrations

--- CM.API/CM.API/Migrations/20241103021709_AddTicketsAvailableToShowtime.Designer.cs
+++ CM.API/CM.API/Migrations/20241103021709_AddTicketsAvailableToShowtime.Designer.cs
@@ -4,6 +4,7 @@ using CM.API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CM.API.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241103021709_AddTicketsAvailableToShowtime")]
+    partial class AddTicketsAvailableToShowtime
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -174,9 +177,6 @@ namespace CM.API.Migrations
 
                     b.Property<decimal>("Price")
                         .HasColumnType("decimal(65,30)");
-
-                    b.Property<int>("Quantity")
-                        .HasColumnType("int");
 
                     b.Property<int>("ShowtimeId")
                         .HasColumnType("int");
---
+++ CM.API/CM.API/Migrations/20241103021709_AddTicketsAvailableToShowtime.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CM.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTicketsAvailableToShowtime : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TicketsAvailable",
+                table: "Showtime",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TicketsAvailable",
+                table: "Showtime");
+        }
+    }
+}
