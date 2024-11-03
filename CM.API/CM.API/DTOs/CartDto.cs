aa135648b28948af2c347be8cf5ff0c596a9acf3
Jackson Williams <wjack1201@yahoo.com>
2024-11-03 11:02:52 -0500

    update Dtos

--- CM.API/CM.API/DTOs/CartDto.cs
+++ CM.API/CM.API/DTOs/CartDto.cs
@@ -3,5 +3,5 @@ public class CartDto
     public int CartId { get; set; }
     public int UserId { get; set; }
     public List<CartTicketDto> Tickets { get; set; } = new List<CartTicketDto>();
-    public decimal TotalPrice => Tickets.Sum(t => t.Price);
+    public decimal TotalPrice => Tickets.Sum(t => t.Price * t.Quantity);
 }
--- CM.API/CM.API/DTOs/CartTicketDto.cs
+++ CM.API/CM.API/DTOs/CartTicketDto.cs
@@ -3,4 +3,6 @@ public class CartTicketDto
     public int Id { get; set; }
     public decimal Price { get; set; }
     public ShowtimeDto Showtime { get; set; }
+    public int Quantity { get; set; }
+
 }
