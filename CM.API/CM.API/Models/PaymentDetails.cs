4f4c0e90f88aaa5949892b36b32f8533303a02c2
Jackson Williams <wjack1201@yahoo.com>
2024-11-05 21:00:54 -0500

    Redudent for now, but will remove later

--- CM.API/CM.API/Models/PaymentDetails.cs
+++ CM.API/CM.API/Models/PaymentDetails.cs
@@ -5,6 +5,7 @@ namespace CM.API.Models;
 public class PaymentDetails
 {
     public int Id { get; set; } // Primary key
+    public int PaymentDetailsId { get; set;}
     public int OrderId { get; set; }
     public decimal Amount { get; set; }
     public string PaymentMethod { get; set; }
