ce63f4fe9963051d637c1b95f5de78c6c2249d42
Jackson Williams <wjack1201@yahoo.com>
2024-11-13 17:26:46 -0500

    Dtos for creation and displaying to client

---
+++ CM.API/CM.API/DTOs/ReviewDto.cs
@@ -0,0 +1,8 @@
+public class ReviewDto
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public int Rating { get; set; } // Rating out of 5
+        public string? Description { get; set; }
+        public int MovieId { get; set; }
+    }
