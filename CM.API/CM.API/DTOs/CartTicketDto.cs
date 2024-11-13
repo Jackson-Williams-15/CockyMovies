a902db4b0ebe2e375798822648e3b81a720735b5
Jackson Williams <wjack1201@yahoo.com>
2024-11-12 23:37:20 -0500

    add movie Id

--- CM.API/CM.API/DTOs/CartTicketDto.cs
+++ CM.API/CM.API/DTOs/CartTicketDto.cs
@@ -1,8 +1,12 @@
+using CM.API.Models;
+
 public class CartTicketDto
 {
     public int Id { get; set; }
     public decimal Price { get; set; }
     public ShowtimeDto Showtime { get; set; }
+    public Movie Movie { get; set; }
+    public int MovieId { get; set;}
     public int Quantity { get; set; }
     public bool IsSold { get; set; }
     public int ShowtimeId { get; set; }
