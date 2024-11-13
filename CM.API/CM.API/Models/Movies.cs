9901f6d0b4b668dea8a3dc02335e75720bccde68
Jackson Williams <wjack1201@yahoo.com>
2024-11-13 17:26:27 -0500

    Relation to movie one-to-many

--- CM.API/CM.API/Models/Movies.cs
+++ CM.API/CM.API/Models/Movies.cs
@@ -7,6 +7,7 @@ public class Movie
     public DateTime DateReleased { get; set; }
     public List<Genre>? Genres { get; set; }
     public required List<Showtime> Showtimes { get; set; }
+    public List<Review>? Reviews { get; set; }
     // Set default image URL
     private string _imageUrl = @"https://plus.unsplash.com/premium_vector-1682303466154-2161da750ac7?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MT
     l8fG1vdmllfGVufDB8fDB8fHww";
