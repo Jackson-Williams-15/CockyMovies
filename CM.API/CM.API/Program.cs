2b8a21788645f2064685354e3a92f267356d404d
Jackson Williams <wjack1201@yahoo.com>
2024-11-15 19:33:24 -0500

    Only use for debugging

--- CM.API/CM.API/Program.cs
+++ CM.API/CM.API/Program.cs
@@ -80,7 +80,8 @@ builder.Services.AddAuthentication(options =>
 
 var app = builder.Build();
 
-app.Use(async (context, next) =>
+// only uncomment for debugging purposes, otherwise leave commented out
+/*app.Use(async (context, next) =>
 {
     // Log the request path and method
     var logger = app.Services.GetRequiredService<ILogger<Program>>();
@@ -93,7 +94,7 @@ app.Use(async (context, next) =>
     logger.LogInformation("Request body: {Body}", body);
 
     await next.Invoke();
-});
+});*/
 
 // Configure cors and frontend URL
 app.UseCors(x => x
