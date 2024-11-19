be2af754ec73dca34b4d977e2dcf0a76ede5d353
Jackson Williams <wjack1201@yahoo.com>
2024-11-18 22:49:06 -0500

    migrations

---
+++ CM.API/CM.API/Migrations/20241118213506_AddPaymentDetailsToUser.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CM.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPaymentDetailsToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PaymentDetailsId",
+                table: "Users",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_PaymentDetailsId",
+                table: "Users",
+                column: "PaymentDetailsId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_PaymentDetails_PaymentDetailsId",
+                table: "Users",
+                column: "PaymentDetailsId",
+                principalTable: "PaymentDetails",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_PaymentDetails_PaymentDetailsId",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_PaymentDetailsId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "PaymentDetailsId",
+                table: "Users");
+        }
+    }
+}
