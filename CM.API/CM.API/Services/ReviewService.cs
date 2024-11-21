67e4af40bb8cbea5c5c698397d0ecf69671674c0
Jackson Williams <wjack1201@yahoo.com>
2024-11-21 14:11:14 -0500

    added some logging

--- CM.API/CM.API/Services/ReviewService.cs
+++ CM.API/CM.API/Services/ReviewService.cs
@@ -5,23 +5,32 @@ using System.Threading.Tasks;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace CM.API.Services;
 
 public class ReviewService : IReviewService
 {
     private readonly AppDbContext _context;
+    private readonly ILogger<ReviewService> _logger;
 
-    public ReviewService(AppDbContext context)
+    public ReviewService(AppDbContext context, ILogger<ReviewService> logger)
     {
         _context = context;
+        _logger = logger;
     }
 
     public async Task<bool> AddReview(Review review)
     {
+        try {
         _context.Reviews.Add(review);
         await _context.SaveChangesAsync();
         return true;
+        }
+        catch(Exception ex) {
+            _logger.LogError(ex, "Error adding review");
+            throw;
+        }
     }
 
     public async Task<List<Review>> GetReviews(int movieId)
