8879b19c21eca3a1a7d359c7ee84dafa9290a478
Jackson Williams <wjack1201@yahoo.com>
2024-11-26 15:14:38 -0500

    Overload methods for different kinds of emails

--- CM.API/CM.API/Interfaces/IEmailService.cs
+++ CM.API/CM.API/Interfaces/IEmailService.cs
@@ -1,4 +1,25 @@
+using CM.API.Models;
+
 public interface IEmailService
 {
+    /// <summary>
+    /// Sends an email with the specified subject and body to the specified recipient.
+    /// General purpose email
+    /// </summary>
+    /// <param name="to">The recipient's email address.</param>
+    /// <param name="subject">The subject of the email.</param>
+    /// <param name="body">The body of the email.</param>
+    /// <returns>A task that represents the asynchronous operation.</returns>
     Task SendEmail(string to, string subject, string body);
+
+    /// <summary>
+    /// Sends an email of the specified type to the specified recipient.
+    /// email for specified type
+    /// </summary>
+    /// <param name="to">The recipient's email address.</param>
+    /// <param name="emailType">The type of email to send.</param>
+    /// <param name="data">The data to include in the email.</param>
+    /// <param name="user">The user associated with the email.</param>
+    /// <returns>A task that represents the asynchronous operation.</returns>
+    Task SendEmail(string to, EmailType emailType, object data, User user);
 }
