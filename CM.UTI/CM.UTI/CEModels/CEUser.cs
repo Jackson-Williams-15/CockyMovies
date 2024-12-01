fc1f2523b3aca3e5663dadf341901b749bd49a17
jssg33 <john@glocation.info>
2024-12-01 12:23:10 -0500

    CM.UTIL Installation & Utilities
    Utilities for CM.

---
+++ CM.UTI/CM.UTI/CEModels/CEUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace cleanweather.Models;
+
+public partial class User
+{
+    public int Id { get; set; }
+
+    public string? Fullname { get; set; }
+
+    public string? Email { get; set; }
+}
---
+++ CM.UTI/CM.UTI/CEModels/Site.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace cleanweather.Models;
+
+public partial class Site
+{
+    public int Id { get; set; }
+
+    public string? Description { get; set; }
+
+    public int? Siteid { get; set; }
+}
