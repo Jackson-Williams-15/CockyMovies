6e50aa5bfceb55c9589accd69c3d1becf03f4240
brennan johnson <brennanmj123@gmail.com>
2024-12-03 13:50:31 -0500

    checkout test added

--- CM.API/CM.API.Tests/Utilities/InMemoryDbContextFactory.cs
+++ CM.API/CM.API.Tests/Utilities/InMemoryDbContextFactory.cs
@@ -2,6 +2,7 @@ using Microsoft.EntityFrameworkCore;
 using CM.API.Data;
 using CM.API.Models;
 using System;
+using System.Collections.Generic;
 
 namespace CM.API.Tests.Utilities
 {
@@ -23,6 +24,7 @@ namespace CM.API.Tests.Utilities
 
         private static void SeedData(AppDbContext context)
         {
+            // Seed user data
             context.Users.Add(new User
             {
                 Id = 1,
@@ -32,6 +34,42 @@ namespace CM.API.Tests.Utilities
                 DateOfBirth = DateTime.Now.AddYears(-25)
             });
 
+            // Seed movie data
+            var movie = new Movie
+            {
+                Id = 1,
+                Title = "Test Movie",
+                Description = "Test Description",
+                DateReleased = DateTime.Now.AddMonths(-2),
+                Rating = new Rating { Id = 1, Name = "PG-13" },
+                Showtimes = new List<Showtime>()
+            };
+            context.Movies.Add(movie);
+
+            // Seed showtime data
+            var showtime = new Showtime
+            {
+                Id = 1,
+                MovieId = movie.Id,
+                StartTime = DateTime.Now,
+                Movie = movie,
+                Tickets = new List<Ticket>()
+            };
+            context.Showtime.Add(showtime);
+
+            // Add showtime to movie
+            movie.Showtimes.Add(showtime);
+
+            // Seed cart data
+            var cart = new Cart
+            {
+                CartId = 1,
+                UserId = 1,
+                Tickets = new List<Ticket>()
+            };
+            context.Carts.Add(cart);
+
+            // Save changes to the in-memory database
             context.SaveChanges();
         }
     }
