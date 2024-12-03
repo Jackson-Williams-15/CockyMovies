922d7cf0f2b41fb83e1db8b9361df1cb133e3f9a
jssg33 <john@glocation.info>
2024-12-03 18:21:14 -0500

    OpenSourceTools(Wordpress Corporate, Worpdress Customer, Joomla Social SSO3.10)
    These are OpenSource Tools

---
+++ CM.UTI/WEBROOT/h/cache/com_content/25e95bf3ab3c5608eac96767c214c99f-cache-com_content-9f50f5b1f61de6fe75df9016b8071620.php
@@ -0,0 +1,72 @@
+<?php die("Access Denied"); ?>#x#a:5:{s:4:"body";s:2350:"<div class="item-page" itemscope itemtype="https://schema.org/Article">
+	<meta itemprop="inLanguage" content="en-GB" />
+		<div class="page-header">
+		<h1> Home </h1>
+	</div>
+	
+		
+			<div class="page-header">
+		<h2 itemprop="headline">
+			Velocity Social Home		</h2>
+							</div>
+							
+<div class="icons">
+	
+					<div class="btn-group pull-right">
+				<button class="btn dropdown-toggle" type="button" id="dropdownMenuButton-1" aria-label="User tools"
+				data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+					<span class="icon-cog" aria-hidden="true"></span>
+					<span class="caret" aria-hidden="true"></span>
+				</button>
+								<ul class="dropdown-menu" aria-labelledby="dropdownMenuButton-1">
+											<li class="print-icon"> <a href="/h/index.php?tmpl=component&amp;print=1" title="Print article < Velocity Social Home >" onclick="window.open(this.href,'win2','status=no,toolbar=no,scrollbars=yes,titlebar=no,menubar=no,resizable=yes,width=640,height=480,directories=no,location=no'); return false;" rel="nofollow">			<span class="icon-print" aria-hidden="true"></span>
+		Print	</a> </li>
+																			</ul>
+			</div>
+		
+	</div>
+			
+		
+						<dl class="article-info muted">
+
+		
+			<dt class="article-info-term">
+									Details							</dt>
+
+							<dd class="createdby" itemprop="author" itemscope itemtype="https://schema.org/Person">
+					Written by <span itemprop="name">Super User</span>	</dd>
+			
+			
+										<dd class="category-name">
+																		Category: <a href="/h/index.php/8-welcome" itemprop="genre">Welcome</a>							</dd>			
+			
+										<dd class="published">
+				<span class="icon-calendar" aria-hidden="true"></span>
+				<time datetime="2024-11-26T02:15:49+00:00" itemprop="datePublished">
+					Published: 26 November 2024				</time>
+			</dd>			
+		
+					
+			
+										<dd class="hits">
+					<span class="icon-eye-open" aria-hidden="true"></span>
+					<meta itemprop="interactionCount" content="UserPageVisits:13" />
+					Hits: 13			</dd>						</dl>
+	
+			
+			<ul class="tags inline">
+																	<li class="tag-2 tag-list0" itemprop="keywords">
+					<a href="/h/index.php/component/tags/tag/home" class="label label-info">
+						Home					</a>
+				</li>
+						</ul>
+	
+		
+								<div itemprop="articleBody">
+		<p><img src="images/cocky-1.png" alt="" /></p> 	</div>
+
+	
+							</div>
+";s:4:"head";a:12:{s:5:"title";s:4:"Home";s:11:"description";N;s:4:"link";s:0:"";s:8:"metaTags";a:2:{s:10:"http-equiv";a:1:{s:12:"content-type";s:24:"text/html; charset=utf-8";}s:4:"name";a:3:{s:8:"keywords";N;s:6:"rights";N;s:6:"author";s:10:"Super User";}}s:5:"links";a:0:{}s:11:"styleSheets";a:0:{}s:5:"style";a:0:{}s:7:"scripts";a:5:{s:29:"/h/media/jui/js/jquery.min.js";a:2:{s:4:"type";s:15:"text/javascript";s:7:"options";a:6:{s:7:"version";s:4:"auto";s:8:"relative";b:1;s:11:"detectDebug";b:0;s:9:"framework";b:0;s:8:"pathOnly";b:0;s:13:"detectBrowser";b:1;}}s:36:"/h/media/jui/js/jquery-noconflict.js";a:2:{s:4:"type";s:15:"text/javascript";s:7:"options";a:6:{s:7:"version";s:4:"auto";s:8:"relative";b:1;s:9:"framework";b:0;s:8:"pathOnly";b:0;s:13:"detectBrowser";b:1;s:11:"detectDebug";b:1;}}s:37:"/h/media/jui/js/jquery-migrate.min.js";a:2:{s:4:"type";s:15:"text/javascript";s:7:"options";a:6:{s:7:"version";s:4:"auto";s:8:"relative";b:1;s:11:"detectDebug";b:0;s:9:"framework";b:0;s:8:"pathOnly";b:0;s:13:"detectBrowser";b:1;}}s:29:"/h/media/system/js/caption.js";a:2:{s:4:"type";s:15:"text/javascript";s:7:"options";a:6:{s:7:"version";s:4:"auto";s:8:"relative";b:1;s:9:"framework";b:0;s:8:"pathOnly";b:0;s:13:"detectBrowser";b:1;s:11:"detectDebug";b:1;}}s:32:"/h/media/jui/js/bootstrap.min.js";a:2:{s:4:"type";s:15:"text/javascript";s:7:"options";a:6:{s:7:"version";s:4:"auto";s:8:"relative";b:1;s:11:"detectDebug";s:1:"0";s:9:"framework";b:0;s:8:"pathOnly";b:0;s:13:"detectBrowser";b:1;}}}s:6:"script";a:1:{s:15:"text/javascript";s:79:"jQuery(window).on('load',  function() {
+				new JCaption('img.caption');
+			});";}s:6:"custom";a:0:{}s:10:"scriptText";a:0:{}s:13:"scriptOptions";a:0:{}}s:13:"mime_encoding";s:9:"text/html";s:7:"pathway";a:0:{}s:6:"module";a:0:{}}
