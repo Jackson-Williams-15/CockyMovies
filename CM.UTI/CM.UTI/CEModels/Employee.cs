62a12b2f4bf38a816ee4e1ddc71095b008d77bd6
jssg33 <john@glocation.info>
2024-12-03 17:38:50 -0500

    Build 2.1
    This is Build2.1 and Just the Executable in Zip form as well.

---
+++ CM.UTI/CM.UTI/CEModels/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace cleanweather.Models;
+
+public partial class Employee
+{
+    public int Id { get; set; }
+
+    public string? Fullname { get; set; }
+
+    public string? Email { get; set; }
+
+    public string? Address1 { get; set; }
+
+    public string? Address2 { get; set; }
+
+    public string? City { get; set; }
+
+    public string? Regionstate { get; set; }
+
+    public int? Zipcode { get; set; }
+}
---
+++ CM.UTI/CM.UTI/MENUSQLDUMP.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore.Query;
+using MySql.Data.MySqlClient;
+using T4DATA;
+using T4DBMYSQL;
+using T4DBMSSQL;
+using T4DATA;
+using T4MYSQLINSTALLER;
+using T4MSSQLINSTALLER;
+using static T4DATA.T4LISTS;
+using Google.Protobuf.Collections;
+
+namespace MENUSYSTEM35
+{
+    public class Menu35
+    {
+
+        // Declare 3 variables - id, age and name 
+        int id;
+        string storecode;
+        string manager;
+        string state;
+
+        public class Product
+        {
+            public int ProductID { get; set; }
+            public string ProductName { get; set; }
+            public decimal Price { get; set; }
+        }
+
+        public override string ToString()
+        {
+            return "LinqSelected:" + id + " " + manager + "        " + storecode + "," + state;
+        }
+
+        // Driver code 
+        static public void DiagMenu()
+        {
+            T4LISTS OurLists2 = new T4LISTS();
+
+            List<Menu35> stores = new List<Menu35>()
+    { 
+          
+        // Create 5 Student details 
+        new Menu35{ id = 101, manager = "Sravan", storecode = "Forrest33", state = "SC" },
+        new Menu35{ id = 102, manager = "deepu", storecode = "Chicago01" , state = "IL"},
+        new Menu35{ id = 103, manager = "manoja", storecode = "Dallas01" , state= "TX"},
+        new Menu35{ id = 104, manager = "Sathwik", storecode = "Sacramento01" ,  state="CA" },
+        new Menu35{ id = 105, manager = "Saran", storecode = "Seatle01", state = "WA" },
+        new Menu35{ id = 106, manager = "Joe", storecode = "Smithfield01", state = "VA" },
+        new Menu35{ id = 107, manager = "April", storecode = "Stohrs01", state = "VT" }
+    };
+
+            List<Product> products = new List<Product>
+        {
+        new Product { ProductName = "Sr. Ticket", Price = 5.99m },
+        new Product { ProductName = "Child Ticket", Price = 4.99m },
+        new Product { ProductName = "Adult Ticket", Price = 8.99m }
+        };
+            int exit = 0;
+            int number = 0;
+            do
+            {
+                Console.WriteLine("\nSystem Utilities: Cocky Movies & Entertainment[V2.1] Installation and Maintenance.");
+                Console.WriteLine("CE Uses a React & ASPX FrontEnd, RESTBackEnd, ASP.NET->LanManager, and COTS Demonstrating The Breadth of Our Team Skills.");
+                Console.WriteLine("Please Enter Your Choice:");
+                Console.WriteLine("9.Diagnostics Processes, and SQL.Utilities");
+                Console.WriteLine("99.Exit:");
+                Console.WriteLine("Please Enter Your Choice(9,99(Exit):\n");
+                string somestring = "1";
+                char myChar = '1';
+                somestring = Console.ReadLine();
+                //myChar = somestring[0];
+                number = Convert.ToInt32(somestring);
+                //string upperstring = somestring.ToUpper();
+                if (number == 99)
+                {
+                    Console.WriteLine("\nYou Choose Option 99: (Exit). You Have Selected to Leave. Thank You.");
+                    Console.WriteLine("\n\n");
+                    exit = 99;
+                }
+                else if (number == 9)
+                {
+                    Console.WriteLine("You Choose Option: 9-UsersBySQL - QUERY DATA\n");
+                    Console.WriteLine("Enter O for OracleMysql, or M for Microsoft Sql Server, E to Exit to Previous Menu");
+                    do
+                    {
+                        somestring = Console.ReadLine();
+                        if (somestring[0] == 'O')
+                        {
+                            T4MYSQL.connectiontest();
+                            exit = 101;
+                        }
+                        else if (somestring[0] == 'M')
+                        {
+                            T4MSSQL.connectiontest();
+                            exit = 101;
+                        }
+                        else if (somestring[0] == 'E')
+                        {
+                            exit = 101;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid Selection. Please Try Again or E to Exit to Previous Menu");
+                        }
+                    } while (exit != 101);
+                    //Console.WriteLine(String.Join("\n", users));
+                    //Console.WriteLine("\n");
+                    exit = 9;
+                }
+                else if (number == 10)
+                {
+                    Console.WriteLine("You Choose Option: 10-Installation\n");
+                    Console.WriteLine("Enter O for OracleMysql, or M for Microsoft Sql Server, E to Exit to Previous Menu");
+                    do
+                    {
+                        somestring = Console.ReadLine();
+                        if (somestring[0] == 'O')
+                        {
+                            MYSQLINSTALLER.createmysqldb();
+                            exit = 101;
+                        }
+                        else if (somestring[0] == 'M')
+                        {
+                            MSSQLINSTALLER.createmssqldb(); 
+                            exit = 101;
+                        }
+                        else if (somestring[0] == 'E')
+                        {
+                            exit = 101;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid Selection. Please Try Again or E to Exit to Previous Menu");
+                        }
+                    } while (exit != 101);
+                    exit = 10;
+                }
+                else if (number == 11)
+                {
+                    Console.WriteLine("You Choose Option: 11-Load Sample Data\n");
+                    //T4MYSQL.connectionload();
+                    //Console.WriteLine(String.Join("\n", users));
+                    //Console.WriteLine("\n");
+                    exit = 11;
+                }
+                else
+                {
+                    Console.WriteLine("\nYou Have Selected to Leave. Thank You.");
+                    Console.WriteLine("\n\n");
+                    exit = 99;
+                }
+                somestring = null;
+            } while (exit != 99);
+
+
+        }
+    }
+}
