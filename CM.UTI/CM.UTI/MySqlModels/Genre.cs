d29e61e5e1296826c60598db268509960ac3378c
jssg33 <john@glocation.info>
2024-12-03 01:59:22 -0500

    Revert "CM.UTIL Installation & Utilities"
    This reverts commit fc1f2523b3aca3e5663dadf341901b749bd49a17.

--- CM.UTI/CM.UTI/MySqlModels/Genre.cs
+++
@@ -1,8 +0,0 @@
-namespace CM.API.Models;
-
-public class Genre
-{
-    public int Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-    public List<Movie> Movies { get; set; } = new();
-}
