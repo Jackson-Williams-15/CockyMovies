d10964b0c1c4f1531ef4db07c58000ce84227ef0
Jackson Williams <wjack1201@yahoo.com>
2024-12-07 12:35:51 -0500

    role added to JWT claims

--- CM.API/CM.API/Controllers/AccountController.cs
+++ CM.API/CM.API/Controllers/AccountController.cs
@@ -127,7 +127,8 @@ public class AccountController : ControllerBase
                 new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString(), user.Username),
                 new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
                 new Claim(ClaimTypes.Name, user.Username),
-                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString())
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Role, user.Role)
             };
 
         var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
