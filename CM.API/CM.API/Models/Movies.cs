ce7b071b7d571528c300c644a9be09fd694d25ae
Jackson Williams <wjack1201@yahoo.com>
2024-12-09 17:53:41 -0500

    required

--- CM.API/CM.API/Models/Movies.cs
+++ CM.API/CM.API/Models/Movies.cs
@@ -28,7 +28,7 @@ public class Movie
     /// <summary>
     /// Gets or sets the list of showtimes for the movie.
     /// </summary>
-    public List<Showtime>? Showtimes { get; set; }
+    public required List<Showtime> Showtimes { get; set; }
     /// <summary>
     /// Gets or sets the list of reviews for the movie.
     /// </summary>
