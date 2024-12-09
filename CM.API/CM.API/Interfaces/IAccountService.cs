c44e25d56c09b12eb25410e7f82f5fefdea2f335
Jackson Williams <wjack1201@yahoo.com>
2024-12-09 02:35:05 -0500

    account null reference check

--- CM.API/CM.API/Interfaces/IAccountService.cs
+++ CM.API/CM.API/Interfaces/IAccountService.cs
@@ -3,10 +3,10 @@ using CM.API.Models;
 namespace CM.API.Interfaces;
 public interface IAccountService
 {
-    Task<UserDto> Authenticate(string username, string password);
+    Task<UserDto?> Authenticate(string username, string password);
     Task<UserDto> Register(string email, string username, string password, DateTime dateOfBirth);
     Task<User> GetUserByUsername(string username);
     Task<User?> GetUserById(string userId);
-    Task<UserDto> UpdateUser(string userId, UserUpdateDto updateDto);
+    Task<UserDto?> UpdateUser(string userId, UserUpdateDto updateDto);
     Task<(bool Success, string Message)> UpdatePassword(string userId, UserPasswordDto userPasswordDto);
 }
--- CM.API/CM.API/Models/Users.cs
+++ CM.API/CM.API/Models/Users.cs
@@ -10,11 +10,11 @@ public class User
 
     [Required(ErrorMessage = "Email is required.")]
     [EmailAddress(ErrorMessage = "Invalid email address.")]
-    public string Email { get; set; }
+    public required string Email { get; set; }
 
     [Required(ErrorMessage = "Username is required.")]
     [StringLength(50, MinimumLength = 3, ErrorMessage = "Username must be between 3 and 50 characters.")]
-    public string Username { get; set; }
+    public required string Username { get; set; }
 
     [Required(ErrorMessage = "Date of Birth is required.")]
     [DataType(DataType.Date)]
@@ -24,7 +24,7 @@ public class User
     [Required(ErrorMessage = "Password is required.")]
     [StringLength(100, MinimumLength = 6, ErrorMessage = "Password must be at least 6 characters long.")]
     [DataType(DataType.Password)]
-    public string Password { get; set; }
+    public string? Password { get; set; }
 
     public static ValidationResult ValidateDateOfBirth(DateTime dateOfBirth, ValidationContext context)
     {
@@ -32,7 +32,7 @@ public class User
         {
             return new ValidationResult("Date of Birth cannot be in the future.");
         }
-        return ValidationResult.Success;
+        return ValidationResult.Success!;
     }
 
     public Cart? Cart { get; set; }
