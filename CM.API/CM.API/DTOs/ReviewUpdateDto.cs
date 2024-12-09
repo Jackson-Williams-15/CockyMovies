d487cc9ff1fb225571acf03603ec5443629f7de8
RAYCOCK1 <raycock@email.sc.edu>
2024-12-08 22:36:30 -0500

    comments added for review related files

--- CM.API/CM.API/DTOs/ReviewUpdateDto.cs
+++ CM.API/CM.API/DTOs/ReviewUpdateDto.cs
@@ -1,13 +1,33 @@
-using System.ComponentModel.DataAnnotations;
-
-public class ReviewUpdateDto
+namespace CM.API.Models
 {
-    [StringLength(100, MinimumLength = 3)]
-    public string? Title { get; set; }
+    // Represents a movie review.
+    public class Review
+    {
+        // Unique identifier for the review.
+        public int Id { get; set; }
 
-    [Range(1, 5)]
-    public int Rating { get; set; } // Rating out of 5
+        // Title of the review.
+        public string? Title { get; set; }
 
-    [StringLength(1000, MinimumLength = 10)]
-    public string? Description { get; set; }
+        // Rating given in the review.
+        public int Rating { get; set; }
+
+        // Detailed description of the review.
+        public string? Description { get; set; }
+
+        // ID of the movie being reviewed.
+        public int MovieId { get; set; }
+
+        // Navigation property for the related Movie.
+        public Movie? Movie { get; set; }
+
+        // Username of the person who wrote the review.
+        public string? Username { get; set; }
+
+        // Number of likes the review has received.
+        public int Likes { get; set; }
+
+        // Collection of replies to the review.
+        public ICollection<Reply>? Reply { get; set; }
+    }
 }
