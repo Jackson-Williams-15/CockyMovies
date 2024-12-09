e3a74f9d18cbf2146f9093d1873ffd0f9b94d8e0
Jackson Williams <wjack1201@yahoo.com>
2024-12-09 02:36:57 -0500

    user null reference check

--- CM.API/CM.API/DTOs/User/UserCreateDto.cs
+++ CM.API/CM.API/DTOs/User/UserCreateDto.cs
@@ -5,11 +5,11 @@ public class UserCreateDto
 {
     [Required(ErrorMessage = "Email is required.")]
     [EmailAddress(ErrorMessage = "Invalid email address.")]
-    public string Email { get; set; }
+    public required string Email { get; set; }
 
     [Required(ErrorMessage = "Username is required.")]
     [StringLength(50, MinimumLength = 3, ErrorMessage = "Username must be between 3 and 50 characters.")]
-    public string Username { get; set; }
+    public required string Username { get; set; }
 
     [Required(ErrorMessage = "Date of Birth is required.")]
     [DataType(DataType.Date)]
@@ -18,5 +18,5 @@ public class UserCreateDto
     [Required(ErrorMessage = "Password is required.")]
     [StringLength(100, MinimumLength = 6, ErrorMessage = "Password must be at least 6 characters long.")]
     [DataType(DataType.Password)]
-    public string Password { get; set; }
+    public required string Password { get; set; }
 }
