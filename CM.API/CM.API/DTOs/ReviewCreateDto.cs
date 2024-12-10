a0fb78e92fdd59e0ade3408661fa2fe53e41580a
Jackson Williams <wjack1201@yahoo.com>
2024-12-09 19:14:45 -0500

    review documentation

--- CM.API/CM.API/DTOs/ReviewCreateDto.cs
+++ CM.API/CM.API/DTOs/ReviewCreateDto.cs
@@ -1,17 +1,36 @@
 using System.ComponentModel.DataAnnotations;
+
+/// <summary>
+/// Data transfer object for creating a review.
+/// </summary>
 public class ReviewCreateDto
 {
+    /// <summary>
+    /// Gets or sets the title of the review.
+    /// </summary>
     [StringLength(100, MinimumLength = 3)]
     public string? Title { get; set; }
 
+    /// <summary>
+    /// Gets or sets the rating of the review (out of 5).
+    /// </summary>
     [Range(1, 5)]
     public int Rating { get; set; } // Rating out of 5
 
+    /// <summary>
+    /// Gets or sets the description of the review.
+    /// </summary>
     [StringLength(1000, MinimumLength = 10)]
     public string? Description { get; set; }
 
+    /// <summary>
+    /// Gets or sets the movie identifier.
+    /// </summary>
     [Required]
     public int MovieId { get; set; }
 
+    /// <summary>
+    /// Gets or sets the username of the reviewer.
+    /// </summary>
     public string? Username { get; set; }
 }
--- CM.API/CM.API/Models/Review.cs
+++ CM.API/CM.API/Models/Review.cs
@@ -1,14 +1,52 @@
 namespace CM.API.Models;
 
+/// <summary>
+/// Represents a review for a movie.
+/// </summary>
 public class Review
 {
+    /// <summary>
+    /// Gets or sets the review identifier.
+    /// </summary>
     public int Id { get; set; }
+
+    /// <summary>
+    /// Gets or sets the title of the review.
+    /// </summary>
     public string? Title { get; set; }
+
+    /// <summary>
+    /// Gets or sets the rating of the review (out of 5).
+    /// </summary>
     public int Rating { get; set; } // Rating out of 5
+
+    /// <summary>
+    /// Gets or sets the description of the review.
+    /// </summary>
     public string? Description { get; set; }
+
+    /// <summary>
+    /// Gets or sets the movie identifier.
+    /// </summary>
     public int MovieId { get; set; }
+
+    /// <summary>
+    /// Gets or sets the movie associated with the review.
+    /// </summary>
     public Movie? Movie { get; set; } // Navigation property
+
+    /// <summary>
+    /// Gets or sets the username of the reviewer.
+    /// </summary>
     public string? Username { get; set; }
+
+    /// <summary>
+    /// Gets or sets the number of likes for the review.
+    /// </summary>
     public int Likes { get; set; }
+
+    /// <summary>
+    /// Gets or sets the collection of replies to the review.
+    /// </summary>
     public ICollection<Reply>? Reply { get; set; }
 }
