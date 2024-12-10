ce7095db12cdfe5978c858fefb820eda5b92d3b9
Jackson Williams <wjack1201@yahoo.com>
2024-12-09 19:17:24 -0500

    reply comments

--- CM.API/CM.API/DTOs/ReplyCreateDto.cs
+++ CM.API/CM.API/DTOs/ReplyCreateDto.cs
@@ -1,6 +1,20 @@
+/// <summary>
+/// Data transfer object for creating a reply.
+/// </summary>
 public class ReplyCreateDto
 {
+    /// <summary>
+    /// Gets or sets the review identifier that this reply is associated with.
+    /// </summary>
     public int ReviewId { get; set; }
+
+    /// <summary>
+    /// Gets or sets the author of the reply.
+    /// </summary>
     public string? Author { get; set; }
+
+    /// <summary>
+    /// Gets or sets the body of the reply.
+    /// </summary>
     public string? Body { get; set; }
 }
